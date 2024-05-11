14781d707bbbeb941437d61c640e9e79727bd461
Raastin-Workspace <152182000+Raastin-Workspace@users.noreply.github.com>
2024-05-12 00:11:27 +0200

    Update JedlixDashboard.py

--- JedlixDashboard.py
+++ JedlixDashboard.py
@@ -575,7 +575,7 @@ def EDA(vars , shorten_columns = False):
     st.write('Summary Statistics')
     cols = st.columns([1,10,1])
     with cols[1]:
-        components.html( summary_styler ,scrolling=True , height= 32* (len(summary_stats )+ 2 ))
+        st.components.v1.html( summary_styler ,scrolling=True , height= 32* (len(summary_stats )+ 2 ))
     
     st.write('Distributions')
     fig = px.histogram(
